f64bb0d4e6151cd307c9bffac8fa730955b19f59
SnehaKarne <65059598+SnehaKarne@users.noreply.github.com>
2020-10-11 21:51:51 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class Emp
+{
+	Scanner sc = new Scanner(System.in);
+	String name;
+	int sal, a_sal;
+	int dob, mob,yob,age;	
+	float tax=0;
+	public void accept()
+	{
+		System.out.println("Enter Name :");
+		name=sc.next();
+		System.out.println("Enter date of Birth :");
+		dob=sc.nextInt();
+		System.out.println("Enter Month of Birth :");
+		mob=sc.nextInt();
+		System.out.println("Enter Year of Birth :");
+		yob=sc.nextInt();
+		System.out.println("Enter Monthly Sallary :");
+		sal=sc.nextInt();
+	}
+
+	public void calculate(){
+		System.out.println("Age : "+(2020-yob)+"Years ");
+		a_sal=sal*12;
+		System.out.println("A_sal :"+a_sal);
+		
+		if(a_sal>=500000){
+			tax=a_sal*20/100;
+			System.out.println("Tax :"+tax);
+		}
+		else if(a_sal<500000 && a_sal>=400000){
+			tax=a_sal*15/100;
+			System.out.println("Tax :"+tax);
+		}
+		else if(a_sal<400000 && a_sal>=300000){
+			tax=a_sal*10/100;
+			System.out.println("Tax :"+tax);
+		}
+		else if(a_sal<300000 && a_sal>=200000){
+			tax=a_sal*5/100;
+			System.out.println("Tax :"+tax);
+		}
+		else {
+			System.out.println("Tax : No Tax");	
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner c=new Scanner(System.in);
+		String ch;
+		Emp e= new Emp();
+		
+		do{
+		e.accept();
+		e.calculate();
+		System.out.println("Do u want to continue ...... press Y/y else N/n");
+		ch=c.next();
+		}while(ch.equals("Y") || ch.equals("y") );
+		
+	}
+}
---
+++ Result.java
@@ -0,0 +1,66 @@
+import java.util.*;
+public class Result
+{
+	Scanner sc = new Scanner(System.in);
+	String name;
+	int m1,m2,m3,m4,m5, tol, per;
+	
+	
+	public void accept()
+	{
+		System.out.println("Enter Name :");
+		name=sc.next();
+		System.out.println("Enter Marks 1 :");
+		m1=sc.nextInt();
+		System.out.println("Enter Marks 2 :");
+		m2=sc.nextInt();
+		System.out.println("Enter Marks 3 :");
+		m3=sc.nextInt();
+		System.out.println("Enter Marks 4 :");
+		m4=sc.nextInt();
+		System.out.println("Enter Marks 5 :");
+		m5=sc.nextInt();
+
+		tol=m1+m2+m3+m4+m5;
+		per=tol/5;
+		
+		if(per<100 && per>=85){
+			System.out.println("Name :"+name);
+			System.out.println("Percentage :"+per);
+			System.out.println("Grade A");
+		}
+		else if(per<85 && per>=70){
+			System.out.println("Name :"+name);
+			System.out.println("Percentage :"+per);
+			System.out.println("Grade B");
+		}
+		else if(per<70 && per>=55){
+			System.out.println("Name :"+name);
+			System.out.println("Percentage :"+per);
+			System.out.println("Grade C");
+		}
+		else if(per<55 && per>=35){
+			System.out.println("Name :"+name);
+			System.out.println("Percentage :"+per);
+			System.out.println("Grade D");
+		}
+		else {
+			System.out.println("Name :"+name);
+			System.out.println("Percentage :"+per);
+			System.out.println("Grade FAIL");
+		}
+	}	
+	
+	public static void main(String args[])
+	{
+		Scanner c=new Scanner(System.in);
+		String ch;
+		Result r= new Result();
+		do{
+		r.accept();
+		System.out.println("Do u want to continue ...... press Y/y else N/n");
+		ch=c.next();
+		}while(ch.equals("Y") || ch.equals("y") );
+		
+	}
+}
